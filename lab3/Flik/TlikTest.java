8bea9bb8c52b2342299e72bebe5e6b17f8130610
eiad loay <loay.eiad@gmail.com>
2024-08-07 18:46:22 +0300

    Finished lab3

---
+++ lab3/Flik/TlikTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TlikTest {
+    @Test
+    public void testIsSameNumber() {
+        Integer a = 10, b = 10;
+        Assert.assertEquals(a, b);
+    }
+}
